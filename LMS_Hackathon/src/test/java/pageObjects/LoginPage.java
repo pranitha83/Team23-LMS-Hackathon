e740e17734489e0ebd1b7ea8e26272f395a5e3a0
Pranitha Bethi <125478903+pranitha83@users.noreply.github.com>
2024-04-24 15:36:24 -0400

    added contextfile

---
+++ LMS_Hackathon/src/test/java/pageObjects/LoginPage.java
@@ -0,0 +1,15 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+
+	private WebDriver driver;
+
+	public LoginPage(WebDriver driver) {
+		this.driver=driver;
+	}
+
+	
+
+}
