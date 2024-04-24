4c3875cab51ec057f04c0aab8d7a16a9b801a26c
Pranitha Bethi <125478903+pranitha83@users.noreply.github.com>
2024-04-24 15:46:32 -0400

    commit generic utils file

---
+++ LMS_Hackathon/src/test/java/pageObjects/PageObjectManager.java
@@ -0,0 +1,26 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+
+public class PageObjectManager {
+
+	public LoginPage loginpage;
+	public WebDriver driver;
+	
+	
+	public PageObjectManager(WebDriver driver) {
+		
+		this.driver=driver;
+		
+	}
+	 
+	public LoginPage getLoginpage() {
+		
+		 loginpage=new LoginPage(driver);
+		 return  loginpage;
+	}
+	
+	
+
+
+}
