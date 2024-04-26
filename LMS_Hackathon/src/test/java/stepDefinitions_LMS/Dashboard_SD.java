5e5a834292fc1fb29ac7ac892575c0451a702ea6
VidhyaM09 <vidhyamanickaraj@gmail.com>
2024-04-25 21:53:32 -0500

    Made dashboard changes

--- LMS_Hackathon/src/test/java/stepDefinitions_LMS/Dashboard_SD.java
+++ LMS_Hackathon/src/test/java/stepDefinitions_LMS/Dashboard_SD.java
@@ -1,5 +1,86 @@
 package stepDefinitions_LMS;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class Dashboard_SD {
 
+	@Then("Admin should see manage program as header")
+	public void admin_should_see_manage_program_as_header() {
+	  
+	}
+
+	@Then("Maximum navigation time in milliseconds, defaults to {int} seconds")
+	public void maximum_navigation_time_in_milliseconds_defaults_to_seconds(Integer int1) {
+	  
+	}
+
+	@Then("HTTP response >= {int}. The link is broken")
+	public void http_response_the_link_is_broken(Integer int1) {
+	   
+	}
+
+	@Then("Admin should see LMS -Learning management system  as title")
+	public void admin_should_see_lms_learning_management_system_as_title() {
+	   
+	}
+
+	@Then("LMS title should be on the top left corner of page")
+	public void lms_title_should_be_on_the_top_left_corner_of_page() {
+	   
+	}
+
+	@Then("Admin should see correct spelling in navigation bar text")
+	public void admin_should_see_correct_spelling_in_navigation_bar_text() {
+	    
+	}
+
+	@Then("Admin should see correct spelling and space in LMS title")
+	public void admin_should_see_correct_spelling_and_space_in_lms_title() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("Admin should see the navigation bar text on the top right side")
+	public void admin_should_see_the_navigation_bar_text_on_the_top_right_side() {
+	  
+	}
+
+	@Then("Admin should see program in the 1st place")
+	public void admin_should_see_program_in_the_1st_place() {
+	    
+	}
+
+	@Then("Admin should see batch in the 2nd place")
+	public void admin_should_see_batch_in_the_2nd_place() {
+	    
+	}
+
+	@Then("Admin should see user in the 3rd place")
+	public void admin_should_see_user_in_the_3rd_place() {
+	    
+	}
+
+	@Then("Admin should see logout in the 4th place")
+	public void admin_should_see_logout_in_the_4th_place() {
+	   
+	}
+
+	@Given("Admin is in dashboard page")
+	public void admin_is_in_dashboard_page() {
+	    
+	}
+
+	@When("Admin click Logout button on navigation bar")
+	public void admin_click_logout_button_on_navigation_bar() {
+	   
+	}
+
+	@Then("Admin should land on login in page")
+	public void admin_should_land_on_login_in_page() {
+	    
+	}
+	
+	
 }
