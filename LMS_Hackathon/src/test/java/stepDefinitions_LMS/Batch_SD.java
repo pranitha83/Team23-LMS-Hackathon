adf97e1e4b6bd7b3e843642fb990e57b53f02362
Rekha Thulasiram <thulrekha@gmail.com>
2024-04-26 19:21:00 -0400

    committing Batch.feature file and Step definition

---
+++ LMS_Hackathon/src/test/java/stepDefinitions_LMS/Batch_SD.java
@@ -0,0 +1,69 @@
+package stepDefinitions_LMS;
+
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.Given;
+
+public class Batch_SD {
+
+    @Given("Admin is on dashboard page after Login")
+    public void admin_is_on_dashboard_page_after_login() {
+
+    }
+
+    @When("Admin clicks Batch from navigation bar")
+    public void admin_clicks_batch_from_navigation_bar() {
+
+    }
+
+    @Then("Admin should see the Manage Batch in the URL")
+    public void admin_should_see_the_manage_batch_in_the_url() {
+
+    }
+
+    @Then("Admin should see the Manage Batch in the header")
+    public void admin_should_see_the_manage_batch_in_the_header() {
+
+    }
+
+    @Then("Admin should see the pagination controls under the data table")
+    public void admin_should_see_the_pagination_controls_under_the_data_table() {
+
+    }
+
+    @Then("Admin Should see the data table with headers Batch name, Batch Description,Batch Status, No. of classes, Program Name, EditDelete")
+    public void admin_should_see_the_data_table_with_headers_batch_name_batch_description_batch_status_no_of_classes_program_name_edit_delete() {
+
+    }
+
+    @Then("Admin should be able to see the {string} icon button that is disabled")
+    public void admin_should_be_able_to_see_the_icon_button_that_is_disabled(String string) {
+
+    }
+
+    @Then("Admin should be able to see the {string} button")
+    public void admin_should_be_able_to_see_the_button(String string) {
+
+    }
+
+    @Then("Each row in the data table should have a checkbox")
+    public void each_row_in_the_data_table_should_have_a_checkbox() {
+
+    }
+
+    @Then("Each row in the data table should have a edit icon that is enabled")
+    public void each_row_in_the_data_table_should_have_a_edit_icon_that_is_enabled() {
+
+    }
+
+    @When("Admin clicks {string} button")
+    public void admin_clicks_button(String string) {
+
+    }
+
+    @Then("A new pop up with Batch details appears")
+    public void a_new_pop_up_with_batch_details_appears() {
+
+    }
+
+}
