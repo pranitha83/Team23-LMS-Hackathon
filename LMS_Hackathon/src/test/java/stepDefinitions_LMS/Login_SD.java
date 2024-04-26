62fe14311955248d657b242e3591e44d7e75bcce
Pranitha Bethi <125478903+pranitha83@users.noreply.github.com>
2024-04-26 15:28:41 -0400

    login with exceldata

--- LMS_Hackathon/src/test/java/stepDefinitions_LMS/Login_SD.java
+++ LMS_Hackathon/src/test/java/stepDefinitions_LMS/Login_SD.java
@@ -1,5 +1,10 @@
 package stepDefinitions_LMS;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.junit.Assert;
 
 import driverFactory.SetupDriver;
@@ -7,6 +12,7 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import pageObjects.LoginPage;
+import utilities.Excel_Reader;
 import utilities.LoggerLoad;
 import utilities.PropertyFileReader;
 import utilities.TestContextSetup;
@@ -15,11 +21,12 @@ public class Login_SD {
 	 LoginPage loginpage;
 	 TestContextSetup context;
 	 SetupDriver setupdriver;
-	 
+	 Excel_Reader excelreader;
 	public Login_SD(TestContextSetup context) {
 		this.context=context;
 		loginpage = context.getpageobjectmanager().getloginpage();
 		setupdriver = context.getsetupdriver();
+		excelreader = context.getexcelreader();
 	}
 
 	
@@ -49,7 +56,7 @@ public class Login_SD {
 	public void admin_enter_valid_credentials_and_clicks_login_button() throws InterruptedException {
 	   loginpage.enterUserNPassword("sdetorganizers@gmail.com", "UIHackathon@02");
 	   loginpage.loginBtnClk();
-	   Thread.sleep(2000);
+	   Thread.sleep(3000);
 	}
 
 	@Then("Admin should land on dashboard page")
@@ -57,6 +64,21 @@ public class Login_SD {
 	    Assert.assertEquals("https://lms-frontend-api-hackathon-apr-326235f3973d.herokuapp.com/", SetupDriver.url());
 		System.out.println(SetupDriver.url());
 	}
+//login credentials from excel
 
-
+	@When("Admin enter valid credentials fromgiven sheetname {string} and rowNumber {int} and clicks login button")
+	public void admin_enter_valid_credentials_fromgiven_sheetname_and_row_number_and_clicks_login_button(String string, Integer int1) throws InvalidFormatException, IOException, InterruptedException {
+	   
+	
+	   List<Map<String, String>>Data=excelreader.getData(PropertyFileReader.getexcelfilepath(), "Login");
+	System.out.println(Data);
+	String userName = Data.get(int1).get("username");
+	String passWord = Data.get(int1).get("password");
+	
+	System.out.println(userName);
+	loginpage.enterUserNPassword(userName, passWord);
+	loginpage.loginBtnClk();
+	Thread.sleep(2000);
+	
+	}
 }
