4bb98de2079f6e649b6ff914a8ccd27325b9fa89
Rekha Thulasiram <thulrekha@gmail.com>
2024-04-26 20:21:52 -0400

    committing BatchPage

---
+++ LMS_Hackathon/src/test/java/pageObjects/BatchPage.java
@@ -0,0 +1,64 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class BatchPage {
+
+    @FindBy (id = "new")  WebElement newBatch;
+
+    @FindBy (id = "edit")  WebElement editBatch;
+
+    @FindBy (id = "filterGlobal") WebElement filterGlobal;
+
+    /*
+    Below elements will be used for New Batch creation
+    and also for updating batch information
+     */
+    @FindBy (id = "batchName") WebElement batchName;
+
+    @FindBy (id = "batchDescription") WebElement batchDescription;
+
+    @FindBy (id = "programName") WebElement programName;
+
+    @FindBy (id = "batchStatus") WebElement batchStatus;
+
+    @FindBy (id = "batchNoOfClasses") WebElement batchNoOfClasses;
+
+    //TODO
+    //SAVE and CANCEL buttons
+
+    WebDriver driver;
+
+
+    public BatchPage(WebDriver driver) {
+        this.driver=driver;
+        PageFactory.initElements(driver, this);
+    }
+
+
+    public void validSearch(String searchString){
+        filterGlobal.sendKeys(searchString);
+    }
+
+    public void addNewBatch(String batchName, String batchDescription, String programName, boolean active, String batchNoOfClasses){
+        this.batchName.sendKeys("batchName");
+        this.batchDescription.sendKeys("batchDescription");
+        this.programName.sendKeys("programName");
+        this.batchStatus.sendKeys("active");
+        this.batchNoOfClasses.sendKeys("batchNoOfClasses");
+
+    }
+
+
+    public void addNewBatchClick(){
+        //this.addNewBatch.click();
+    }
+
+    public void editBatchClick(){
+        //this.editBatch.click();
+    }
+
+}
