7b35b4dcafcd6a032504b6ec8f27b1d3bb9de85b
Pranitha Bethi <125478903+pranitha83@users.noreply.github.com>
2024-04-28 06:37:19 -0400

    changed utils

--- LMS_Hackathon/src/test/java/utilities/GenericUtils.java
+++ LMS_Hackathon/src/test/java/utilities/GenericUtils.java
@@ -1,23 +1,11 @@
 package utilities;
 
-
 import org.openqa.selenium.WebDriver;
-
-import driverFactory.SetupDriver;
-
 
 public class GenericUtils {
 	
-	 TestContextSetup context;
-	 SetupDriver setupdriver;
-	  public WebDriver driver;
-     
-      
-	public GenericUtils(TestContextSetup context) {
-		
-		this.context=context;
-		this.driver=SetupDriver.Driver();
-		
+	public GenericUtils(WebDriver driver) {
+		// TODO Auto-generated constructor stub
 }
 		
 	}
