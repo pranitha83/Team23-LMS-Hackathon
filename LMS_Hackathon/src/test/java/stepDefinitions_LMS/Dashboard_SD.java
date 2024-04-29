1b6b07dff18d257247e3179037f3b3f88c60d896
VidhyaM09 <vidhyamanickaraj@gmail.com>
2024-04-28 21:11:20 -0500

    dashboard commit

--- LMS_Hackathon/src/test/java/stepDefinitions_LMS/Dashboard_SD.java
+++ LMS_Hackathon/src/test/java/stepDefinitions_LMS/Dashboard_SD.java
@@ -11,31 +11,28 @@ import utilities.PropertyFileReader;
 import utilities.TestContextSetup;
 
 public class Dashboard_SD {
-	
-	
-	 DashboardPage dashboardpage;
-	 TestContextSetup context;
-	 SetupDriver setupdriver;
 
-	 
+	DashboardPage dashboardpage;
+	TestContextSetup context;
+	SetupDriver setupdriver;
+
 	public Dashboard_SD(TestContextSetup context) {
-		this.context=context;
+		this.context = context;
 		dashboardpage = context.getpageobjectmanager().getdashboardpage();
 		setupdriver = context.getsetupdriver();
-		
-		
+
 	}
-	
+
 	@Then("Admin should see manage program as {string} header")
 	public void admin_should_see_manage_program_as_header(String header) throws InterruptedException {
-		
-		  Thread.sleep(3000);
-		  LoggerLoad.info("Validate admin sees manage program as header"); String
-		  dashboardHeader = dashboardpage.verifyHeaderText();
-		  Assert.assertEquals(dashboardHeader, header);
-		 
+
+		Thread.sleep(3000);
+		LoggerLoad.info("Validate admin sees manage program as header");
+		String dashboardHeader = dashboardpage.verifyHeaderText();
+		Assert.assertEquals(dashboardHeader, header);
+
 	}
-		
+
 	@Then("Maximum navigation time in milliseconds, defaults to {int} seconds")
 	public void maximum_navigation_time_in_milliseconds_defaults_to_seconds(Integer int1) throws InterruptedException {
 		LoggerLoad.info("Validate navigation time");
@@ -69,7 +66,7 @@ public class Dashboard_SD {
 
 	@Then("Admin should see correct spelling and space in LMS title")
 	public void admin_should_see_correct_spelling_and_space_in_lms_title() {
-	    LoggerLoad.info("Validate LMS Title Spelling and Space");
+		LoggerLoad.info("Validate LMS Title Spelling and Space");
 		dashboardpage.title_SpellcheckAndSpacecheck();
 	}
 
@@ -107,7 +104,7 @@ public class Dashboard_SD {
 	public void admin_is_in_dashboard_page() throws Throwable {
 		LoggerLoad.info("Validate admin is on dashboard page");
 		Thread.sleep(2000);
-		Assert.assertEquals(PropertyFileReader.getGlobalValue("dashboardurl"),setupdriver.url());
+		Assert.assertEquals(PropertyFileReader.getGlobalValue("dashboardurl"), setupdriver.url());
 	}
 
 	@When("Admin click Logout button on navigation bar")
@@ -121,34 +118,46 @@ public class Dashboard_SD {
 		LoggerLoad.info("Validate admin is on login page");
 		Assert.assertEquals(PropertyFileReader.getGlobalValue("loginpageurl"), setupdriver.url());
 	}
-	
+
 	@Then("Admin should see data table on the Manage Program Page with following column headers \\(Program Name, Program Description, Program Status, Edit,Delete)")
 	public void admin_should_see_data_table_on_the_manage_program_page_with_following_column_headers_program_name_program_description_program_status_edit_delete() {
-	   dashboardpage.verifyCoulmnHeaders();
+		dashboardpage.verifyCoulmnHeaders();
 	}
-	
+
 	@Then("Admin should see the sort arrow icon beside to each column header except Edit and Delete")
 	public void admin_should_see_the_sort_arrow_icon_beside_to_each_column_header_except_edit_and_delete() {
-	   dashboardpage.sortIconsValidation();
+		dashboardpage.sortIconsValidation();
 	}
 
 	@Then("Admin should see check box on the left side in all rows of the data table")
 	public void admin_should_see_check_box_on_the_left_side_in_all_rows_of_the_data_table() {
-	    dashboardpage.checkboxesValidation();
+		dashboardpage.checkboxesValidation();
 	}
-	
+
 	@Then("Admin should see the Edit and Delete buttons on each row of the data table")
 	public void admin_should_see_the_edit_and_delete_buttons_on_each_row_of_the_data_table() {
-	  dashboardpage.editDeleteButtonsValidation();
+		dashboardpage.editDeleteButtonsValidation();
 	}
-	
+
 	@Then("Admin should see Search bar with text as {string}")
 	public void admin_should_see_search_bar_with_text_as(String search) {
-	    dashboardpage.validateSearchTextBox(search);
+		dashboardpage.validateSearchTextBox(search);
 	}
-	
+
 	@Then("Admin should see the number of records \\(rows of data in the table) displayed on the page are {int}")
-	public void admin_should_see_the_number_of_records_rows_of_data_in_the_table_displayed_on_the_page_are(Integer totalrows) {
-	   dashboardpage.validateTotalRowsDisplayed(totalrows);
+	public void admin_should_see_the_number_of_records_rows_of_data_in_the_table_displayed_on_the_page_are(
+			Integer totalrows) {
+		dashboardpage.validateTotalRowsDisplayed(totalrows);
+	}
+
+	@Then("Admin should see a {string} button with plus icon on the program page above the data table")
+	public void admin_should_see_a_button_with_plus_icon_on_the_program_page_above_the_data_table(
+			String newProgramIcon) {
+		dashboardpage.validateNewProgramButton(newProgramIcon);
+	}
+
+	@Then("Admin should see a Delete button on the top left hand side as Disabled")
+	public void admin_should_see_a_delete_button_on_the_top_left_hand_side_as_disabled() {
+		dashboardpage.validateMultipleProgramDeleteBtnDisabled();
 	}
 }
